15831c3ea9d07fab4100ab60f513d1734af542c8
Alexei Garrow <alexei.garrow@gmail.com>
2024-04-24 16:10:00 +1200

    add pseudocode

---
+++ main.js
@@ -0,0 +1,39 @@
+//create a variable entries (they've used an array) I'm guessing this is where we type things to be
+//calculated.
+
+//There is a variable for the total, they've used a 0;
+
+//A temporary variable string? $ ? is a concatonation, insert a piece of script.
+//This has has a onclick, text insert function. Creates intteractivity
+
+//This next bit grabs a number and adds it to the temp, checks via an if and a === for '.' does this mean decimal number?
+//It then concatonates. Maybe if decimal do this, otherwise reset? So maybe
+//It's like if it's a number not sure on the "." else clear / reset
+//We are making buttons on the calculator.
+//So the next one is the CE button //clear last entry. Take you back one.
+
+//eval() method? Can add a string together?!?
+
+//They then change the multiply symbol to work with the eval method?>
+
+//The same with divide
+
+//Create the equals '=' button? Seems like a for loop something like this, taken from
+//stack overflow:
+// var operators = {
+//   '+': function(x, y) { return x + y; },
+//   '-': function(x, y) { return x - y; },
+//   '*': function(x, y) { return x * y; },
+//   '/': function(x, y) { return x / y; }
+//};
+//
+//
+//Under this swap the - symbol so it handles correctly?
+
+//answer entry, a new blank array and a temp
+
+//else push number to temp and val (with a new empty temp string)
+
+//Math.abs should be super useful
+
+//This outline is like one giant function? We could make smaller ones.
