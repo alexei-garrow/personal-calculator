778f50127b40b1368fa63a81f2a809bc1075539e
Alexei Garrow <alexei.garrow@gmail.com>
2024-08-12 00:33:00 +1200

    npm wtf

---
+++ main.js
@@ -0,0 +1,19 @@
+//use eval() so we don't do the calculations ourselves
+//Getting the string out of the calculator, displaying
+//going to need 2 variables
+//be able to type and or use the buttons.
+
+let displayedNumber = document.getElementById('result')
+let scope = eval
+
+//appending to our current displayed number
+
+function dis(value) {
+  displayedNumber.value += value //displayerNumber is an object therefore has a value. Element is the HTML word for obj
+}
+
+function clr() {
+  displayedNumber.value = '' //empty string not a number
+}
+
+function solve() {}
